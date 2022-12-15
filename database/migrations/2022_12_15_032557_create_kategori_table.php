2d7229a608f9d596273ea2453d9680a5e544374c
lutf87 <luthfiarrahman007@gmail.com>
2022-12-15 10:31:36 +0700

    database kategori 2

---
+++ database/migrations/2022_12_15_032557_create_kategori_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKategoriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategori', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kode_kategori');
+            $table->string('nama_kategori');
+            $table->string('slug_kategori');
+            $table->text('deskripsi_kategori');
+            $table->string('status');
+            $table->string('foto')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategori');
+    }
+}
